000794fe774e86cea56958c527f7ea173516d9ac
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-01-09 14:46:46 +0200

    Create MainWrapper.js

---
+++ client/shop-for-you/src/components/MainWrapper/MainWrapper.js
@@ -0,0 +1,13 @@
+import { Header } from "../common/Header/Header";
+import { Welcome } from "../common/Welcome/Welcome";
+
+export const MainWrapper = () => {
+  return (
+    <>
+      <Header />
+      <main id="main">
+        <Welcome />
+      </main>
+    </>
+  );
+};
