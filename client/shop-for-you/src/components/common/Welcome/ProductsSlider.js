d2d9ae31f68f86c9c712b67bbe59d6f96135dad6
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-01-09 14:46:49 +0200

    Create ProductsSlider.js

---
+++ client/shop-for-you/src/components/common/Welcome/ProductsSlider.js
@@ -0,0 +1,338 @@
+export const ProductsSlider = () => {
+  return (
+    <section className="products-slider">
+      <div className="products-container">
+        <article className="women-products-header">
+          <h3>Women's</h3>
+          <h1>New Products</h1>
+        </article>
+        <div className="women-products-container">
+          <article className="product-card">
+            <div className="card-img-container">
+              <img
+                className="prod-img"
+                src="./images/nike-product.jpg"
+                alt=""
+              />
+            </div>
+            <div className="product-title">
+              <a href="/">Product title</a>
+              <h3>39.99$</h3>
+            </div>
+            <div className="sizes">
+              <p className="size transition">
+                <span>S</span>
+              </p>
+              <p className="size transition">
+                <span>M</span>
+              </p>
+              <p className="size transition">
+                <span>L</span>
+              </p>
+              <p className="size transition">
+                <span>XL</span>
+              </p>
+            </div>
+          </article>
+          <article className="product-card">
+            <div className="card-img-container">
+              <img
+                className="prod-img"
+                src="./images/nike-product.jpg"
+                alt=""
+              />
+            </div>
+            <div className="product-title">
+              <a href="/">Product title</a>
+              <h3>39.99$</h3>
+            </div>
+            <div className="sizes">
+              <p className="size">
+                <span>S</span>
+              </p>
+              <p className="size">
+                <span>M</span>
+              </p>
+              <p className="size">
+                <span>L</span>
+              </p>
+              <p className="size">
+                <span>XL</span>
+              </p>
+            </div>
+          </article>
+          <article className="product-card">
+            <div className="card-img-container">
+              <img
+                className="prod-img"
+                src="./images/nike-product.jpg"
+                alt=""
+              />
+            </div>
+            <div className="product-title">
+              <a href="/">Product title</a>
+              <h3>39.99$</h3>
+            </div>
+            <div className="sizes">
+              <p className="size">
+                <span>S</span>
+              </p>
+              <p className="size">
+                <span>M</span>
+              </p>
+              <p className="size">
+                <span>L</span>
+              </p>
+              <p className="size">
+                <span>XL</span>
+              </p>
+            </div>
+          </article>
+          <article className="product-card">
+            <div className="card-img-container">
+              <img
+                className="prod-img"
+                src="./images/nike-product.jpg"
+                alt=""
+              />
+            </div>
+            <div className="product-title">
+              <a href="/">Product title</a>
+              <h3>39.99$</h3>
+            </div>
+            <div className="sizes">
+              <p className="size">
+                <span>S</span>
+              </p>
+              <p className="size">
+                <span>M</span>
+              </p>
+              <p className="size">
+                <span>L</span>
+              </p>
+              <p className="size">
+                <span>XL</span>
+              </p>
+            </div>
+          </article>
+          <article className="product-card">
+            <div className="card-img-container">
+              <img
+                className="prod-img"
+                src="./images/nike-product.jpg"
+                alt=""
+              />
+            </div>
+            <div className="product-title">
+              <a href="/">Product title</a>
+              <h3>39.99$</h3>
+            </div>
+            <div className="sizes">
+              <p className="size">
+                <span>S</span>
+              </p>
+              <p className="size">
+                <span>M</span>
+              </p>
+              <p className="size">
+                <span>L</span>
+              </p>
+              <p className="size">
+                <span>XL</span>
+              </p>
+            </div>
+          </article>
+          <article className="product-card">
+            <div className="card-img-container">
+              <img
+                className="prod-img"
+                src="./images/nike-product.jpg"
+                alt=""
+              />
+            </div>
+            <div className="product-title">
+              <a href="/">Product title</a>
+              <h3>39.99$</h3>
+            </div>
+            <div className="sizes">
+              <p className="size">
+                <span>S</span>
+              </p>
+              <p className="size">
+                <span>M</span>
+              </p>
+              <p className="size">
+                <span>L</span>
+              </p>
+              <p className="size">
+                <span>XL</span>
+              </p>
+            </div>
+          </article>
+          <article className="product-card">
+            <div className="card-img-container">
+              <img
+                className="prod-img"
+                src="./images/nike-product.jpg"
+                alt=""
+              />
+            </div>
+            <div className="product-title">
+              <a href="/">Product title</a>
+              <h3>39.99$</h3>
+            </div>
+            <div className="sizes">
+              <p className="size">
+                <span>S</span>
+              </p>
+              <p className="size">
+                <span>M</span>
+              </p>
+              <p className="size">
+                <span>L</span>
+              </p>
+              <p className="size">
+                <span>XL</span>
+              </p>
+            </div>
+          </article>
+          <article className="product-card">
+            <div className="card-img-container">
+              <img
+                className="prod-img"
+                src="./images/nike-product.jpg"
+                alt=""
+              />
+            </div>
+            <div className="product-title">
+              <a href="/">Product title</a>
+              <h3>39.99$</h3>
+            </div>
+            <div className="sizes">
+              <p className="size">
+                <span>S</span>
+              </p>
+              <p className="size">
+                <span>M</span>
+              </p>
+              <p className="size">
+                <span>L</span>
+              </p>
+              <p className="size">
+                <span>XL</span>
+              </p>
+            </div>
+          </article>
+          <article className="product-card">
+            <div className="card-img-container">
+              <img
+                className="prod-img"
+                src="./images/nike-product.jpg"
+                alt=""
+              />
+            </div>
+            <div className="product-title">
+              <a href="/">Product title</a>
+              <h3>39.99$</h3>
+            </div>
+            <div className="sizes">
+              <p className="size">
+                <span>S</span>
+              </p>
+              <p className="size">
+                <span>M</span>
+              </p>
+              <p className="size">
+                <span>L</span>
+              </p>
+              <p className="size">
+                <span>XL</span>
+              </p>
+            </div>
+          </article>
+          <article className="product-card">
+            <div className="card-img-container">
+              <img
+                className="prod-img"
+                src="./images/nike-product.jpg"
+                alt=""
+              />
+            </div>
+            <div className="product-title">
+              <a href="/">Product title</a>
+              <h3>39.99$</h3>
+            </div>
+            <div className="sizes">
+              <p className="size">
+                <span>S</span>
+              </p>
+              <p className="size">
+                <span>M</span>
+              </p>
+              <p className="size">
+                <span>L</span>
+              </p>
+              <p className="size">
+                <span>XL</span>
+              </p>
+            </div>
+          </article>
+          <article className="product-card">
+            <div className="card-img-container">
+              <img
+                className="prod-img"
+                src="./images/nike-product.jpg"
+                alt=""
+              />
+            </div>
+            <div className="product-title">
+              <a href="/">Product title</a>
+              <h3>39.99$</h3>
+            </div>
+            <div className="sizes">
+              <p className="size">
+                <span>S</span>
+              </p>
+              <p className="size">
+                <span>M</span>
+              </p>
+              <p className="size">
+                <span>L</span>
+              </p>
+              <p className="size">
+                <span>XL</span>
+              </p>
+            </div>
+          </article>
+          <article className="product-card">
+            <div className="card-img-container">
+              <img
+                className="prod-img"
+                src="./images/nike-product.jpg"
+                alt=""
+              />
+            </div>
+            <div className="product-title">
+              <a href="/">Product title</a>
+              <h3>39.99$</h3>
+            </div>
+            <div className="sizes">
+              <p className="size">
+                <span>S</span>
+              </p>
+              <p className="size">
+                <span>M</span>
+              </p>
+              <p className="size">
+                <span>L</span>
+              </p>
+              <p className="size">
+                <span>XL</span>
+              </p>
+            </div>
+          </article>
+        </div>
+      </div>
+    </section>
+  );
+};
