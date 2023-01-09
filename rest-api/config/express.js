f4082fc360d046457505dc8565c3802624ca3c87
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-01-09 13:33:46 +0200

    Create express.js

---
+++ rest-api/config/express.js
@@ -0,0 +1,20 @@
+const cookieParser = require("cookie-parser");
+const cloudinary = require("cloudinary").v2;
+
+const config = require("./config");
+const cors = require("../Middlewares/cors");
+const { isAuth } = require("../Middlewares/isAuthMiddleware");
+
+function expressConfig(app, express) {
+  cloudinary.config(config.CLOUDINARY_CONFIG);
+
+  app.use(cors(config.CORS.origin));
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+  app.use(cookieParser());
+  app.use(isAuth);
+
+  //   app.use(express.static(__dirname + "/public"));
+}
+
+module.exports = expressConfig;
