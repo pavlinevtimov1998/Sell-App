4ccbbcf7c5b4621fbf20aad42c39f3d3db365f45
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-01-09 13:33:43 +0200

    Create config.js

---
+++ rest-api/config/config.js
@@ -0,0 +1,39 @@
+const env = "DEVELOPMENT";
+
+// process.env.NODE_ENV || 
+
+const config = {
+  DEVELOPMENT: {
+    PORT: process.env.PORT || 3030,
+    DB_URL: "mongodb://localhost:27017/shop-for-you",
+    // DB_URL: process.env.DB_URL.replace("<PASSWORD>", process.env.DB_PASS), // enter your local MongoDB string here
+    COOKIE_NAME: process.env.COOKIE_NAME || "token", // enter your cookie name here
+    SECRET: process.env.JWT_SECRET || "very secret word", //enter your secret here
+    SALT_ROUNDS: process.env.SALT || 10,
+    CLOUDINARY_CONFIG: {
+      cloud_name: process.env.CLOUD_NAME, // enter your cloud name from cloudinary account
+      api_key: process.env.API_KEY, // enter your cloud key from cloudinary account
+      api_secret: process.env.API_SECRET, // enter your cloud secret from cloudinary account
+    },
+    CORS: {
+      origin: "http://localhost:3000",
+    },
+  },
+  // PRODUCTION: {
+  //   PORT: process.env.PORT,
+  //   DB_URL: process.env.DB_URL.replace("<PASSWORD>", process.env.DB_PASS),
+  //   COOKIE_NAME: process.env.COOKIE_NAME,
+  //   SECRET: process.env.JWT_SECRET,
+  //   SALT_ROUNDS: process.env.SALT,
+  //   CLOUDINARY_CONFIG: {
+  //     cloud_name: process.env.CLOUD_NAME,
+  //     api_key: process.env.API_KEY,
+  //     api_secret: process.env.API_SECRET,
+  //   },
+  //   CORS: {
+  //     origin: "",
+  //   },
+  // },
+};
+
+module.exports = config[env];
