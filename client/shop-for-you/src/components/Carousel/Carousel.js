2f84c9233ef9a30dc58343056d90a11ee4010e97
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-01-09 14:46:57 +0200

    Create Carousel.js

---
+++ client/shop-for-you/src/components/Carousel/Carousel.js
@@ -0,0 +1,17 @@
+export const Carousel = () => {
+  return (
+    <section id="carousel">
+      <div className="images-container">
+        <img
+          src="/images/desktop-middle-banner-1704x740-24ff0036277417190c7425279caa0e76.jpg"
+          alt=""
+          className="img"
+        />
+        <img src="/images/nike.jpg" alt="" className="img" />
+        <img src="/images/adidas-shoes.jpg" alt="" className="img" />
+        <img src="/images/banner.jpg" alt="" className="img" />
+        <img src="/images/nike-shoes.jpg" alt="" className="img" />
+      </div>
+    </section>
+  );
+};
