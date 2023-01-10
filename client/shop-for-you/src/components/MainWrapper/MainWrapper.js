8633481817f9e0aa7ddf92439684ef9da646822d
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-01-10 21:27:46 +0200

    add footer

--- client/shop-for-you/src/components/MainWrapper/MainWrapper.js
+++ client/shop-for-you/src/components/MainWrapper/MainWrapper.js
@@ -1,3 +1,4 @@
+import { Footer } from "../common/Footer/Footer";
 import { Header } from "../common/Header/Header";
 import { Welcome } from "../common/Welcome/Welcome";
 
@@ -8,6 +9,7 @@ export const MainWrapper = () => {
       <main id="main">
         <Welcome />
       </main>
+      <Footer />
     </>
   );
 };
