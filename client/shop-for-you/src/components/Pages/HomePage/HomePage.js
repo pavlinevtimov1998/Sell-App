20161fbb0f94654af03054e9498ea012066c072a
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-01-11 01:21:05 +0200

    folder changes

--- client/shop-for-you/src/components/Pages/HomePage/HomePage.js
+++ client/shop-for-you/src/components/Pages/HomePage/HomePage.js
@@ -3,14 +3,14 @@ import { Categories } from "./Categories/Categories";
 import { ProductsSlider } from "./ProductsSlider/ProductsSlider";
 import { Subscribe } from "../../Subscribe/Subscribe";
 
-export const Welcome = () => {
-  return (
-    <>
-      <Carousel />
-      <ProductsSlider />
-      <Categories />
-      <ProductsSlider />
-      <Subscribe />
-    </>
-  );
+export const HomePage = () => {
+    return (
+        <>
+            <Carousel />
+            <ProductsSlider />
+            <Categories />
+            <ProductsSlider />
+            <Subscribe />
+        </>
+    );
 };
