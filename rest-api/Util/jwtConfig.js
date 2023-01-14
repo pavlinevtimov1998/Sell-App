3b606417759ecf3fe7831be8f6384c4027c632af
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-01-14 21:25:38 +0200

    Update jwtConfig.js

--- rest-api/Util/jwtConfig.js
+++ rest-api/Util/jwtConfig.js
@@ -4,15 +4,15 @@ const { promisify } = require("util");
 const config = require("../config/config");
 
 exports.getToken = (payload) => {
-  const jwtSignPromise = promisify(jwt.sign);
+    const jwtSignPromise = promisify(jwt.sign);
 
-  return jwtSignPromise(payload, config.SECRET, {
-    expiresIn: "1d",
-  });
+    return jwtSignPromise(payload, config.SECRET, {
+        expiresIn: "1d",
+    });
 };
 
 exports.verifyToken = (token) => {
-  const jwtVerify = promisify(jwt.verify);
+    const jwtVerify = promisify(jwt.verify);
 
-  return jwtVerify(token, config.SECRET);
+    return jwtVerify(token, config.SECRET);
 };
