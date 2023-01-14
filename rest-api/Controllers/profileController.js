7377559ac2d47af78593e7db2df504c9ccaa8956
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-01-14 21:28:29 +0200

    Create profileController.js

---
+++ rest-api/Controllers/profileController.js
@@ -0,0 +1,50 @@
+const profileController = require("express").Router();
+
+const profileService = require("../Services/profileService");
+const { isUser } = require("../Middlewares/guards");
+const { catchAsyncError } = require("../Util/errorParser");
+const { upload } = require("../Util/imageUpload");
+
+profileController.get(
+    "/",
+    isUser,
+    catchAsyncError(async (req, res) => {
+        const profileData = await profileService.getProfileData(req.user._id);
+
+        res.json(profileData);
+    })
+);
+
+profileController.patch(
+    "/",
+    isUser,
+    catchAsyncError(async (req, res) => {
+        const userId = req.user._id;
+
+        const profileData = await profileService.editProfileData(
+            userId,
+            req.body
+        );
+
+        res.json(profileData);
+    })
+);
+
+profileController.patch(
+    "/image-update",
+    isUser,
+    upload.single("image"),
+    catchAsyncError(async (req, res) => {
+        const userId = req.user._id;
+        const image = req.file;
+
+        const profileData = await profileService.updateProfileImg(
+            userId,
+            image
+        );
+
+        res.json({ image: profileData.userInfo.image });
+    })
+);
+
+module.exports = profileController;
