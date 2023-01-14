f9b27fcdde508ef810e61cfac13673f617267981
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-01-14 22:40:26 +0200

    Update router.js

--- rest-api/router.js
+++ rest-api/router.js
@@ -3,9 +3,16 @@ const router = require("express").Router();
 const authController = require("./Controllers/authController");
 const productController = require("./Controllers/productsController");
 const categoriesController = require("./Controllers/categoriesController");
+const profileController = require("./Controllers/profileController");
+
+const { globalErrorHandler } = require("./Middlewares/globallErrHandler");
 
 router.use("/auth", authController);
+router.use("/profile", profileController);
+
 router.use("/products", productController);
 router.use("/categories", categoriesController);
+
+router.use(globalErrorHandler);
 
 module.exports = router;
