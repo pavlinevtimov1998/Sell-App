9ba0fe375bc96b405478b1f4dcf7622ae268a105
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-01-14 21:31:31 +0200

    Update isAuthMiddleware.js

--- rest-api/Middlewares/isAuthMiddleware.js
+++ rest-api/Middlewares/isAuthMiddleware.js
@@ -2,27 +2,24 @@ const config = require("../config/config");
 const { verifyToken } = require("../Util/jwtConfig");
 
 exports.isAuth = async (req, res, next) => {
-  const token = req.cookies[config.COOKIE_NAME];
+    const token = req.cookies[config.COOKIE_NAME];
 
-  if (!token) {
-    return next();
-  }
-
-  try {
-    const payload = await verifyToken(token);
-
-    if (!payload) {
-      return next();
+    if (!token) {
+        return next();
     }
 
-    if (payload.isAgency) {
-      req.agency = payload;
-    } else {
-      req.user = payload;
+    try {
+        const payload = await verifyToken(token);
+
+        if (!payload) {
+            return next();
+        }
+
+        req.user = payload;
+
+        next();
+    } catch (err) {
+        res.clearCookie(config.COOKIE_NAME);
+        res.status(401).json({ message: "Token expired!" });
     }
-    next();
-  } catch (err) {
-    res.clearCookie(config.COOKIE_NAME);
-    res.status(401).json({ message: "Token expired!" });
-  }
 };
