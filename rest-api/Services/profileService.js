88db7b797b6dae1622b5e8a2cf5809a622771bf6
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-01-14 21:27:21 +0200

    add edit profile data

--- rest-api/Services/profileService.js
+++ rest-api/Services/profileService.js
@@ -5,6 +5,11 @@ const { getImagesUrl, deleteCloudinaryImage } = require("../Util/imageUpload");
 const getProfileData = (userId) =>
     User.findById(userId).select("-password -__v -updatedAt").lean();
 
+    
+const editProfileData = (userId, body) =>
+User.findByIdAndUpdate(userId, body, { new: true, runValidators: true });
+
+
 
 module.exports = {
     getProfileData,
