91c73e9696cdeb154b9734bb7a95145224c06549
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-01-14 21:31:14 +0200

    Update authController.js

--- rest-api/Controllers/authController.js
+++ rest-api/Controllers/authController.js
@@ -1,19 +1,21 @@
 const authController = require("express").Router();
 
 const config = require("../config/config");
+const authService = require("../Services/authService");
 const { catchAsyncError } = require("../Util/errorParser");
 
 authController.post(
     "/login",
     catchAsyncError(async (req, res) => {
-        const [token, user] = await userService.login(req.body);
+        const [token, user] = await authService.login(req.body);
 
         res.cookie(config.COOKIE_NAME, token, { httpOnly: true });
 
         res.status(200).json({
             _id: user._id,
-            username: user.username,
-            // image: user.image,
+            email: user.email,
+            userInfo: user.userInfo,
+            isAdmin: user.isAdmin,
         });
     })
 );
@@ -21,19 +23,20 @@ authController.post(
 authController.post(
     "/register",
     catchAsyncError(async (req, res) => {
-        const [token, user] = await userService.register(req.body);
+        const [token, user] = await authService.register(req.body);
 
         res.cookie(config.COOKIE_NAME, token, { httpOnly: true });
 
         res.status(201).json({
             _id: user._id,
-            username: user.username,
-            // image: user.image,
+            email: user.email,
+            userInfo: user.userInfo,
+            isAdmin: user.isAdmin,
         });
     })
 );
 
-userController.post("/logout", (req, res) => {
+authController.post("/logout", (req, res) => {
     res.clearCookie(config.COOKIE_NAME);
     res.status(200).json({ message: "Successfull logout!" });
 });
