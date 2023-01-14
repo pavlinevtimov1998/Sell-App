6e11b8f18ef11e314d3b274d1db8d6980d7d76a8
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-01-14 21:27:54 +0200

    add update user image functionality

--- rest-api/Services/profileService.js
+++ rest-api/Services/profileService.js
@@ -5,11 +5,32 @@ const { getImagesUrl, deleteCloudinaryImage } = require("../Util/imageUpload");
 const getProfileData = (userId) =>
     User.findById(userId).select("-password -__v -updatedAt").lean();
 
-    
 const editProfileData = (userId, body) =>
-User.findByIdAndUpdate(userId, body, { new: true, runValidators: true });
+    User.findByIdAndUpdate(userId, body, { new: true, runValidators: true });
 
+async function updateProfileImg(userId, file) {
+    let cloudinaryImageId;
 
+    const [newImage, profileData] = await Promise.all([
+        getImagesUrl(file),
+        User.findById(userId),
+    ]);
+
+    const { image } = profileData.userInfo;
+
+    if (image) {
+        cloudinaryImageId = profileData.userInfo.image.substring(
+            profileData.userInfo.image.lastIndexOf("/") + 1,
+            profileData.userInfo.image.lastIndexOf(".")
+        );
+
+        await deleteCloudinaryImage(cloudinaryImageId);
+    }
+
+    profileData.userInfo.image = newImage;
+
+    return profileData.save();
+}
 
 module.exports = {
     getProfileData,
