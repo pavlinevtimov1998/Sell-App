70cbdefae70b9e2e7f08fde794f7da1f22a08592
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-01-16 23:49:38 +0200

    change file paths

---
+++ rest-api/Utils/errorParser.js
@@ -0,0 +1,36 @@
+const config = require("../config/config");
+
+exports.catchAsyncError = function (func) {
+    return (req, res, next) => {
+        func(req, res, next).catch((err) => {
+            if (err.name == "CastError") {
+                return next({ message: "Not Found!", status: 404 });
+            } else if (err.code == "11000") {
+                const message = handleDuplicateError(err);
+
+                return next({ message, status: 400 });
+            } else if (err.name == "ValidationError") {
+                const message = handleValidationError(err);
+
+                return next({ message, status: 400 });
+            } else if (err.message == "Token expired!") {
+                res.clearCookie(config.COOKIE_NAME);
+                return next({ message: "Unauthorized!", status: 401 });
+            } else {
+                return next(err);
+            }
+        });
+    };
+};
+
+const handleValidationError = (err) => {
+    const errors = Object.values(err.errors).map((el) => el.message);
+
+    return `Incorrect input. ${errors.join(". ")}`;
+};
+
+const handleDuplicateError = (err) => {
+    let value = Object.values(err.keyValue)[0];
+
+    return `${value} already exists. Please try with different!`;
+};
