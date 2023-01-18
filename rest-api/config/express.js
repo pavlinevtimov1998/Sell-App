b86e0250cbc00370101a6b5711f2f38875662e1d
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-01-18 12:03:35 +0200

    Update express.js

--- rest-api/config/express.js
+++ rest-api/config/express.js
@@ -5,16 +5,19 @@ const config = require("./config");
 const cors = require("../Middlewares/cors");
 const { isAuth } = require("../Middlewares/isAuthMiddleware");
 
+const router = require("../router");
+
 function expressConfig(app, express) {
-  cloudinary.config(config.CLOUDINARY_CONFIG);
+    cloudinary.config(config.CLOUDINARY_CONFIG);
 
-  app.use(cors(config.CORS.origin));
-  app.use(express.urlencoded({ extended: true }));
-  app.use(express.json());
-  app.use(cookieParser());
-  app.use(isAuth);
+    app.use(cors(config.CORS.origin));
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
+    app.use(cookieParser());
+    app.use(isAuth);
 
-  //   app.use(express.static(__dirname + "/public"));
+    app.use("/api", router);
+    //   app.use(express.static(__dirname + "/public"));
 }
 
 module.exports = expressConfig;
