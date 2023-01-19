700b70cdf68f9815efe87a61828a09c68c17f1ad
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-01-19 23:42:43 +0200

    add create category endpoint

--- rest-api/Controllers/categoriesController.js
+++ rest-api/Controllers/categoriesController.js
@@ -1,7 +1,9 @@
 const categoriesController = require("express").Router();
 
+const { isAdmin } = require("../Middlewares/guards");
 const categoriesService = require("../Services/categoriesService");
 const { catchAsyncError } = require("../Utils/errorParser");
+const { upload } = require("../Utils/imageUpload");
 
 categoriesController.get(
     "/",
@@ -9,6 +11,25 @@ categoriesController.get(
         const categories = await categoriesService.getAllCategories();
 
         res.status(200).json(categories);
+    })
+);
+
+categoriesController.post(
+    "/",
+    isAdmin("Unauthorized!"),
+    upload.single("image"),
+    catchAsyncError(async (req, res) => {
+        const categoryData = req.body;
+        const file = req.file;
+
+        const category = await categoriesService.createCategory(
+            categoryData,
+            file
+        );
+
+        res.status(200).json({
+            message: `Successfully created ${category.title} category!`,
+        });
     })
 );
 
