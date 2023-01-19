ca2695d850a02fead46132b78c344988c1d47cb1
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-01-19 12:11:45 +0200

    Update Subcategory.js

--- rest-api/Models/Subcategory.js
+++ rest-api/Models/Subcategory.js
@@ -4,6 +4,9 @@ const subcategorySchema = new mongoose.Schema({
     title: {
         type: String,
     },
+    image: {
+        type: String,
+    },
     products: [
         {
             type: mongoose.Types.ObjectId,
