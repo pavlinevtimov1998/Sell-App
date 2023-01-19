3ee0988cfbb8e4c5140fdcc56761d69aa66ee324
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-01-19 12:11:47 +0200

    Update Category.js

--- rest-api/Models/Category.js
+++ rest-api/Models/Category.js
@@ -1,17 +1,20 @@
 const mongoose = require("mongoose");
 
 const categorySchema = new mongoose.Schema({
-  title: {
-    type: String,
-  },
-  subcategories: [
-    {
-      type: String,
-      ref: "Subcategory",
-      default: [],
-      required: true,
+    title: {
+        type: String,
     },
-  ],
+    image: {
+        type: String,
+    },
+    subcategories: [
+        {
+            type: String,
+            ref: "Subcategory",
+            default: [],
+            required: true,
+        },
+    ],
 });
 
 const Category = mongoose.model("Category", categorySchema);
