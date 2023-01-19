2389335cf4d1acce9b36aa67ef198e2a6e4d2b6a
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-01-19 23:42:54 +0200

    add create category functionality

--- rest-api/Services/categoriesService.js
+++ rest-api/Services/categoriesService.js
@@ -1,7 +1,22 @@
 const Category = require("../Models/Category");
+const { getImagesUrl } = require("../Utils/imageUpload");
 
 const getAllCategories = () => Category.find();
 
+async function createCategory(body, file) {
+    if (!file) {
+        throw {
+            message: "Category image is required!",
+            status: 400,
+        };
+    }
+
+    body.image = await getImagesUrl(file);
+
+    return Category.create(body);
+}
+
 module.exports = {
     getAllCategories,
+    createCategory,
 };
