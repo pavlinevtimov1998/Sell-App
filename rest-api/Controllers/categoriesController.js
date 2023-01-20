8483a2c3ebb88288bbdd0b588b7c450c94e873fd
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-01-20 13:32:39 +0200

    Update categoriesController.js

--- rest-api/Controllers/categoriesController.js
+++ rest-api/Controllers/categoriesController.js
@@ -22,7 +22,7 @@ categoriesController.post(
         const categoryData = req.body;
         const file = req.file;
 
-        const category = await categoriesService.categoryAction(
+        const category = await categoriesService.createCategory(
             categoryData,
             file
         );
@@ -42,7 +42,7 @@ categoriesController.patch(
         const file = req.file;
         const categoryId = req.params.categoryId;
 
-        await categoriesService.categoryAction(
+        await categoriesService.editCategory(
             categoryData,
             file,
             true,
