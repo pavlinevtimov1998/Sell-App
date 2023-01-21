26f5937b693df2f0e41162b8bc436fa1dd340fa3
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-01-21 13:40:48 +0200

    Update router.js

--- rest-api/router.js
+++ rest-api/router.js
@@ -4,6 +4,7 @@ const authController = require("./Controllers/authController");
 const productController = require("./Controllers/productsController");
 const categoriesController = require("./Controllers/categoriesController");
 const profileController = require("./Controllers/profileController");
+const subcategoriesController = require("./Controllers/subcategoryController");
 
 const { globalErrorHandler } = require("./Middlewares/globallErrHandler");
 
@@ -12,6 +13,7 @@ router.use("/profile", profileController);
 
 router.use("/products", productController);
 router.use("/categories", categoriesController);
+router.use("/subcategories", subcategoriesController);
 
 router.use(globalErrorHandler);
 
