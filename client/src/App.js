b33d76a2ff6f6654bac895b09a839a7430598ae6
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-01-22 13:12:02 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,15 @@ import "./App.css";
 
 import { Footer } from "./Components/Core/Footer/Footer";
 import { Header } from "./Components/Core/Header/Header";
+import { SearchBar } from "./Components/SearchBar/SearchBar";
+import { Carousel } from "./Components/Carousel/Carousel";
 
 function App() {
     return (
         <>
             <Header />
-
+            <SearchBar />
+            <Carousel />
             <main className="main"></main>
 
             <Footer />
