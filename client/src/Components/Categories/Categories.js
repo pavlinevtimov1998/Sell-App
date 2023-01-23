bba3c9b0e7afb760757eed44521d1d1e974662f3
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-01-23 21:10:59 +0200

    Update Categories.js

--- client/src/Components/Categories/Categories.js
+++ client/src/Components/Categories/Categories.js
@@ -1,29 +1,21 @@
-import { useFetch } from "../../Hooks/useFetch";
-
 import styles from "./Categories.module.css";
-import { getAll } from "../../Services/categoriesService";
 
 import { CategoryItem } from "../CategoryItem/CategoryItem";
 
-export const Categories = () => {
-    const { isLoading, data, error } = useFetch(getAll);
-
-    console.log(isLoading, data, error);
-
+export const Categories = ({ data }) => {
     return (
         <section className={styles["categories"]}>
             <header className={styles["categories-header"]}>
                 <h1 className={styles["title"]}>Categories</h1>
             </header>
             <div className={styles["categories-container"]}>
-                {data &&
-                    data.map((c) => (
-                        <CategoryItem
-                            key={c._id}
-                            category={c}
-                            subcategories={c.subcategories}
-                        />
-                    ))}
+                {data.map((c) => (
+                    <CategoryItem
+                        key={c._id}
+                        category={c}
+                        subcategories={c.subcategories}
+                    />
+                ))}
             </div>
         </section>
     );
