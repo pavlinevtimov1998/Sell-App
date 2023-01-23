78fc098e1aa1f64625f9a1dff4f51004612abd59
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-01-23 13:14:48 +0200

    add arrow down

--- client/src/Components/CategoryItem/CategoryItem.js
+++ client/src/Components/CategoryItem/CategoryItem.js
@@ -7,10 +7,17 @@ export const CategoryItem = ({ category, subcategories }) => {
     return (
         <Link className={styles["item"]}>
             <div className={styles["image-container"]}>
-                <img className={styles["img"]} src={category.image} alt={category.title} />
+                <img
+                    className={styles["img"]}
+                    src={category.image}
+                    alt={category.title}
+                />
             </div>
             <div className={styles["category-title"]}>
                 <span>{category.title}</span>
+                <svg className={styles["arrow-down"]} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                    <path d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z" />
+                </svg>
             </div>
         </Link>
     );
