611c917888843bfb9e0c1694abe1f0a56ad0b04b
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-01-24 00:50:59 +0200

    Create AuthContext.js

---
+++ client/src/Contexts/AuthContext.js
@@ -0,0 +1,17 @@
+import { createContext, useState } from "react";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+
+    const handleLogin = (userData) => setUser(userData);
+
+    const handleLogout = () => setUser(null);
+
+    return (
+        <AuthContext.Provider value={{ user, handleLogin, handleLogout }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
