69e356b8903c3cac6d75e9864fe799317b21d80f
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-01-24 01:41:25 +0200

    Update userService.js

--- client/src/Services/userService.js
+++ client/src/Services/userService.js
@@ -1,7 +1,10 @@
 import * as apiService from "./apiService";
 
 const endpoints = {
+    userData: "/auth/user-data",
     login: "/auth/login",
 };
+
+export const initialRequest = () => apiService.getRequest(endpoints.userData);
 
 export const login = (data) => apiService.postRequest(endpoints.login, data);
