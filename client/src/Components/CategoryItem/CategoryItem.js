4129a015005380256d7f8a38c4b4856406b91136
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-01-23 12:58:45 +0200

    add styles to categories components

--- client/src/Components/CategoryItem/CategoryItem.js
+++ client/src/Components/CategoryItem/CategoryItem.js
@@ -1,15 +1,17 @@
 import styles from "./CategoryItem.module.css";
 
+import { Link } from "react-router-dom";
+
 export const CategoryItem = ({ category, subcategories }) => {
     console.log(subcategories);
     return (
-        <a className={styles["item"]} href={styles["item"]}>
+        <Link className={styles["item"]}>
             <div className={styles["image-container"]}>
-                <img src={category.image} alt={category.title} />
+                <img className={styles["img"]} src={category.image} alt={category.title} />
             </div>
             <div className={styles["category-title"]}>
                 <span>{category.title}</span>
             </div>
-        </a>
+        </Link>
     );
 };
