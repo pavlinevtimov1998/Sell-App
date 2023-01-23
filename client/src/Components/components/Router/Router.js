e7c516e7774c08a427ce79b73a76306a8bb55c9f
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-01-23 23:59:40 +0200

    Update Router.js

--- client/src/Components/components/Router/Router.js
+++ client/src/Components/components/Router/Router.js
@@ -1,13 +1,18 @@
 import { Route, Routes } from "react-router-dom";
+import { AuthPage } from "../../Pages/AuthPage/AuthPage";
 
 import { HomePage } from "../../Pages/HomePage/HomePage";
 
 export const AppRouter = () => {
     return (
         <Routes>
-
             <Route element={<HomePage />} path="/" />
-            
+
+            <Route element={<AuthPage action={"login"} />} path="/login" />
+            <Route
+                element={<AuthPage action={"register"} />}
+                path="/register"
+            />
         </Routes>
     );
 };
