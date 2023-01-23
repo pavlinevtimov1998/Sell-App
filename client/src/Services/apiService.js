df64364b50c5618686922d6fc9352b4abbfb4eaa
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-01-23 14:58:02 +0200

    Create apiService.js

---
+++ client/src/Services/apiService.js
@@ -0,0 +1,32 @@
+const API_URL = "http://localhost:3030/api";
+
+async function request(url, options) {
+    const response = await fetch(`${API_URL}/${url}`, options);
+
+    return response.json();
+}
+
+function createOptions(method = "GET", data) {
+    const options = {
+        method,
+        credentials: "include",
+        headers: {},
+    };
+
+    if (data) {
+        options.headers["Content-Type"] = "application/json";
+        options["body"] = JSON.stringify(data);
+    }
+
+    return options;
+}
+
+export const getRequest = (url) => request(url, createOptions("GET"));
+
+export const postRequest = (url, data) =>
+    request(url, createOptions("POST", data));
+
+export const patchRequest = (url, data) =>
+    request(url, createOptions("PATCH", data));
+
+export const deleteRequest = (url) => request(url, createOptions("DELETE"));
