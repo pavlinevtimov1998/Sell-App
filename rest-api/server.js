937874203cce63a3007276c01833153f53323494
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-01-23 13:55:42 +0200

    Update server.js

--- rest-api/server.js
+++ rest-api/server.js
@@ -9,12 +9,16 @@ const { DB_URL, PORT } = require("./config/config");
 const expressConfig = require("./config/express");
 const { initDB } = require("./config/database");
 
+const router = require("./router");
+
 async function startServer() {
     const app = express();
 
     await initDB(DB_URL);
 
     expressConfig(app, express);
+
+    app.use("/api", router);
 
     // app.use("*", (req, res) => {
     //   res.sendFile(__dirname + "/public/index.html");
