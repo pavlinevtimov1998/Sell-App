2e8c7f06f57a639afada21bfc5c51e70b8ec75f7
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-01-24 12:40:47 +0200

    Update MainWrapper.js

--- client/src/Components/components/MainWrapper/MainWrapper.js
+++ client/src/Components/components/MainWrapper/MainWrapper.js
@@ -1,24 +1,5 @@
-import { useContext, useEffect } from "react";
-import { AuthContext } from "../../../Contexts/AuthContext";
-
-import { initialRequest } from "../../../Services/userService";
-
 import { AppRouter } from "../Router/Router";
 
 export const MainWrapper = () => {
-    const { handleLogin } = useContext(AuthContext);
-
-    useEffect(() => {
-        initialRequest()
-            .then((result) => {
-                if (!result.message) {
-                    handleLogin(result);
-                }
-            })
-            .catch((err) => console.log(err.message));
-
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-
     return <AppRouter />;
 };
