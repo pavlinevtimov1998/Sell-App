60b4a2d049336f0aefe1756ce3ad9aa84edb2d59
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-01-28 00:10:51 +0200

    Update MainWrapper.js

--- client/src/Components/components/MainWrapper/MainWrapper.js
+++ client/src/Components/components/MainWrapper/MainWrapper.js
@@ -1,5 +1,31 @@
+import { useEffect, useRef, useState } from "react";
+import { ErrorContext } from "../../../Contexts/ErrorContext";
+
+import { ErrorMessage } from "../ErrorMessage/ErrorMessage";
 import { AppRouter } from "../Router/Router";
 
 export const MainWrapper = () => {
-    return <AppRouter />;
+    const [error, setError] = useState({ message: "", hasError: false });
+    const errorRef = useRef();
+
+    useEffect(() => {
+        let id;
+
+        if (error.hasError) {
+            errorRef.current.style.right = "40px";
+            id = setInterval(() => {
+                setError((state) => ({ ...state, hasError: false }));
+            }, 5000);
+        } else {
+            errorRef.current.style.right = "-300px";
+            id && clearInterval(id);
+        }
+    }, [error]);
+
+    return (
+        <ErrorContext.Provider value={{ setError }}>
+            <ErrorMessage message={error.message} errorRef={errorRef} />
+            <AppRouter />;
+        </ErrorContext.Provider>
+    );
 };
