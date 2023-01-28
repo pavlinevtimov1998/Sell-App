4c9a73e47b0056aa141592b8c405cdce3832948c
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-01-28 12:25:18 +0200

    Update CategoryItem.js

--- client/src/Components/components/CategoryItem/CategoryItem.js
+++ client/src/Components/components/CategoryItem/CategoryItem.js
@@ -2,6 +2,7 @@ import styles from "./CategoryItem.module.css";
 
 import { useState } from "react";
 import { SubcategoriesDropdown } from "../SubcategoriesDropdown/SubcategoriesDropdown";
+import { Link } from "react-router-dom";
 
 export const CategoryItem = ({ category, subcategories }) => {
     const [isDropdownVisible, setIsDropdownVisible] = useState(false);
@@ -13,35 +14,34 @@ export const CategoryItem = ({ category, subcategories }) => {
     const isVisible = isDropdownVisible ? "open" : "hide";
 
     return (
-        <button
-            onMouseEnter={toggleSubcategoryMenu}
+        <div
             onMouseLeave={toggleSubcategoryMenu}
-            type="button"
-            title={category.title}
-            className={`${styles["item"]}`}
+            onMouseEnter={toggleSubcategoryMenu}
+            className={styles["item-wrapper"]}
         >
-            <div className={styles["image-container"]}>
-                <img
-                    className={styles["img"]}
-                    src={category.image}
-                    alt={category.title}
-                />
-            </div>
-            <div className={styles["category-title"]}>
-                <span>{category.title}</span>
-                <svg
-                    className={styles["arrow-down"]}
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 320 512"
-                >
-                    <path d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z" />
-                </svg>
-
-                <SubcategoriesDropdown
-                    subcategories={subcategories}
-                    isVisible={isVisible}
-                />
-            </div>
-        </button>
+            <Link title={category.title} className={`${styles["item"]}`}>
+                <div className={styles["image-container"]}>
+                    <img
+                        className={styles["img"]}
+                        src={category.image}
+                        alt={category.title}
+                    />
+                </div>
+                <div className={styles["category-title"]}>
+                    <span>{category.title}</span>
+                    <svg
+                        className={styles["arrow-down"]}
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 320 512"
+                    >
+                        <path d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z" />
+                    </svg>
+                </div>
+            </Link>{" "}
+            <SubcategoriesDropdown
+                subcategories={subcategories}
+                isVisible={isVisible}
+            />
+        </div>
     );
 };
