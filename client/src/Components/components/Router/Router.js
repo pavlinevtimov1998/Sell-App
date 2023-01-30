6e83263055baccfea0f3bdcfc86e543902c194fb
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-01-30 14:44:01 +0200

    Update Router.js

--- client/src/Components/components/Router/Router.js
+++ client/src/Components/components/Router/Router.js
@@ -4,6 +4,7 @@ import { HomePage } from "../../Pages/HomePage/HomePage";
 import { AuthPage } from "../../Pages/AuthPage/AuthPage";
 import { Logout } from "../Logout/Logout";
 import { DetailsPage } from "../../Pages/DetailsPage/DetailsPage";
+import { ProductActionPage } from "../../Pages/ProductActionPage/ProductActionPage";
 
 export const AppRouter = () => {
     return (
@@ -11,6 +12,11 @@ export const AppRouter = () => {
             <Route element={<HomePage />} path="/" />
 
             <Route element={<DetailsPage />} path="/product/:productId" />
+
+            <Route
+                element={<ProductActionPage action="create" />}
+                path="/create-product"
+            />
 
             <Route element={<AuthPage action={"login"} />} path="/login" />
             <Route
