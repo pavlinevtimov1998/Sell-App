6a35312410f8d7d81653bf166aed043b84cc2769
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-02-02 23:59:59 +0200

    Update productsService.js

--- client/src/Services/productsService.js
+++ client/src/Services/productsService.js
@@ -3,9 +3,13 @@ import * as apiService from "./apiService";
 const endpoints = {
     getLast: "/products?limit=16",
     getOne: (productId) => `/products/${productId}`,
+    create: "/products",
 };
 
 export const getLastProducts = () => apiService.getRequest(endpoints.getLast);
 
 export const getOneProduct = (productId) =>
     apiService.getRequest(endpoints.getOne(productId));
+
+export const createProduct = (data) =>
+    apiService.postRequest(endpoints.create, data, true);
