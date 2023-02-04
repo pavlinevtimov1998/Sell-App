f7ccc50f84af9ec311358b4c3da6f667239f3149
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-02-04 17:27:27 +0200

    Update RegisterForm.js

--- client/src/Components/components/AuthForms/RegisterForm.js
+++ client/src/Components/components/AuthForms/RegisterForm.js
@@ -9,6 +9,7 @@ import { register } from "../../../Services/userService";
 import styles from "./AuthForms.module.css";
 
 import * as validators from "../../../Utils/validators";
+import { FormButton } from "../FormButton/FormButton";
 
 export const RegisterForm = () => {
     const { handleLogin } = useContext(AuthContext);
@@ -178,16 +179,16 @@ export const RegisterForm = () => {
                     <a href="/">Forgot password?</a>
                 </div> */}
                 <div className={styles["btn-container"]}>
-                    <button className={styles["btn"]} type="submit">
+                    <FormButton content="Register">
                         <svg
-                            className={styles["btn-icon"]}
+                            width={20}
+                            height={20}
                             xmlns="http://www.w3.org/2000/svg"
                             viewBox="0 0 512 512"
                         >
                             <path d="M352 96h64c17.7 0 32 14.3 32 32V384c0 17.7-14.3 32-32 32H352c-17.7 0-32 14.3-32 32s14.3 32 32 32h64c53 0 96-43 96-96V128c0-53-43-96-96-96H352c-17.7 0-32 14.3-32 32s14.3 32 32 32zm-7.5 177.4c4.8-4.5 7.5-10.8 7.5-17.4s-2.7-12.9-7.5-17.4l-144-136c-7-6.6-17.2-8.4-26-4.6s-14.5 12.5-14.5 22v72H32c-17.7 0-32 14.3-32 32v64c0 17.7 14.3 32 32 32H160v72c0 9.6 5.7 18.2 14.5 22s19 2 26-4.6l144-136z" />
                         </svg>
-                        <span>Login</span>
-                    </button>
+                    </FormButton>
                 </div>
             </div>
         </form>
