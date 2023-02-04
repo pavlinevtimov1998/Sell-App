eba46c1026897ee2c5adb9cf87ab33c0e0ddee7a
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-02-04 11:19:45 +0200

    Update isAuthMiddleware.js

--- client/src/Utils/util.js
+++ client/src/Utils/util.js
@@ -1,2 +1,10 @@
 export const trim = (text, index) =>
     text.length - 3 > index ? text.substring(0, index) + "..." : text;
+
+export const dateParser = (date) => {
+    const day = new Date(date).getDate();
+    const year = new Date(date).getFullYear();
+    const month = new Date(date).getMonth() + 1;
+
+    return `${year}/${month}/${day}`;
+};
