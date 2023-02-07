b5c1998540665c48829bd7412e82fcbe9e481e32
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-02-07 12:59:28 +0200

    Update FormButton.js

--- client/src/Components/components/FormButton/FormButton.js
+++ client/src/Components/components/FormButton/FormButton.js
@@ -1,9 +1,9 @@
 import styles from "./FormButton.module.css";
 
-export const FormButton = ({ content, children }) => {
+export const FormButton = ({ content, children, className, type }) => {
     return (
-        <button className={styles["btn"]} type="submit">
-            <span className={styles["sub-btn-content"]}>{content}</span>
+        <button className={className} type={type || "submit"}>
+            <span className={styles["btn-content"]}>{content}</span>
             {children}
         </button>
     );
