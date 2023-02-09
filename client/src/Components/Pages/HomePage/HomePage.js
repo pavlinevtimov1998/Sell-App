025ad6e4bb607a0717a15daca12d798bd48f986c
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-02-10 00:15:28 +0200

    Update HomePage.js

--- client/src/Components/Pages/HomePage/HomePage.js
+++ client/src/Components/Pages/HomePage/HomePage.js
@@ -7,16 +7,33 @@ import { ProductsList } from "../../components/ProductsList/ProductsList";
 
 import { getAllCategories } from "../../../Services/categoriesService";
 import { getLastProducts } from "../../../Services/productsService";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const getData = () => Promise.all([getAllCategories(), getLastProducts()]);
 
 export const HomePage = () => {
     const { isLoading, data } = useFetch(getData);
+    const [isSearchBarVisible, setIsSearchBarVisible] = useState(
+        window.innerWidth > 600
+    );
 
     useEffect(() => {
         window.scrollTo({ top: 0, behavior: "smooth" });
         document.title = "Welcome Page";
+    }, []);
+
+    useEffect(() => {
+        const resizeHandler = (e) => {
+            if (e.target.innerWidth > 600) {
+                setIsSearchBarVisible(true);
+            } else {
+                setIsSearchBarVisible(false);
+            }
+        };
+
+        window.addEventListener("resize", resizeHandler);
+
+        return () => window.removeEventListener("resize", resizeHandler);
     }, []);
 
     return (
@@ -25,7 +42,7 @@ export const HomePage = () => {
                 <Spinner />
             ) : (
                 <>
-                    <SearchBar />
+                    {isSearchBarVisible && <SearchBar />}
                     <Categories data={data[0]} />
                     <ProductsList products={data[1]} />
                 </>
