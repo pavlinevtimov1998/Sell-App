566eb1b474ef5a9e063692b75ec1a274da613160
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-02-10 13:24:22 +0200

    Update HomePage.js

--- client/src/Components/Pages/HomePage/HomePage.js
+++ client/src/Components/Pages/HomePage/HomePage.js
@@ -8,32 +8,17 @@ import { ProductsList } from "../../components/ProductsList/ProductsList";
 import { getAllCategories } from "../../../Services/categoriesService";
 import { getLastProducts } from "../../../Services/productsService";
 import { useEffect, useState } from "react";
+import { useResize } from "../../../Hooks/useResize";
 
 const getData = () => Promise.all([getAllCategories(), getLastProducts()]);
 
 export const HomePage = () => {
     const { isLoading, data } = useFetch(getData);
-    const [isSearchBarVisible, setIsSearchBarVisible] = useState(
-        window.innerWidth > 600
-    );
+    const { state: isMobile } = useResize(600);
 
     useEffect(() => {
         window.scrollTo({ top: 0, behavior: "smooth" });
         document.title = "Welcome Page";
-    }, []);
-
-    useEffect(() => {
-        const resizeHandler = (e) => {
-            if (e.target.innerWidth > 600) {
-                setIsSearchBarVisible(true);
-            } else {
-                setIsSearchBarVisible(false);
-            }
-        };
-
-        window.addEventListener("resize", resizeHandler);
-
-        return () => window.removeEventListener("resize", resizeHandler);
     }, []);
 
     return (
@@ -42,7 +27,7 @@ export const HomePage = () => {
                 <Spinner />
             ) : (
                 <>
-                    {isSearchBarVisible && <SearchBar />}
+                    {!isMobile && <SearchBar />}
                     <Categories data={data[0]} />
                     <ProductsList products={data[1]} />
                 </>
