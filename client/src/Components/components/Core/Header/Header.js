55efacd7d6fe23fe7e8edba5a906043e5017ea08
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-02-10 14:12:15 +0200

    add mobile nav

--- client/src/Components/components/Core/Header/Header.js
+++ client/src/Components/components/Core/Header/Header.js
@@ -1,10 +1,11 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../../../Contexts/AuthContext";
 import { useHeaderVisibility } from "../../../../Hooks/useHeaderVisibility";
 import { useResize } from "../../../../Hooks/useResize";
 
 import styles from "./Header.module.css";
+import { MobileNav } from "./MobileNav/MobileNav";
 
 import { GuestOptions } from "./UserNav/GuestOptions";
 import { UserOptions } from "./UserNav/UserOptions";
@@ -13,32 +14,48 @@ export const Header = () => {
     const { isHeaderVisible } = useHeaderVisibility();
     const { state: isMobile } = useResize(600);
     const { userData } = useContext(AuthContext);
+    const [isMobileNavVisible, setisMobileNavVisible] = useState(false);
 
     const headerClass = isHeaderVisible ? "show" : "hide";
 
+    const mobileNavHandler = () => setisMobileNavVisible((state) => !state);
+
     return (
-        <header className={`${styles["header"]} ${styles[headerClass]}`}>
-            <nav className={styles["nav-bar"]}>
-                <article className={styles["header-logo"]}>
-                    <Link to="/" className={styles["logo"]}>
-                        <img src="/logo.png" alt="LOGO" />
-                    </Link>
-                </article>
-                {userData && !isMobile && <UserOptions />}
-                {!userData && !isMobile && <GuestOptions />}
-                {isMobile && (
-                    <button type="button" className={styles["mobile-nav-btn"]}>
-                        <svg
-                            width={30}
-                            height={30}
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 448 512"
+        <>
+            <header className={`${styles["header"]} ${styles[headerClass]}`}>
+                <nav className={styles["nav-bar"]}>
+                    <article className={styles["header-logo"]}>
+                        <Link to="/" className={styles["logo"]}>
+                            <img src="/logo.png" alt="LOGO" />
+                        </Link>
+                    </article>
+                    {userData && !isMobile && <UserOptions />}
+                    {!userData && !isMobile && <GuestOptions />}
+                    {isMobile && (
+                        <button
+                            onClick={mobileNavHandler}
+                            type="button"
+                            className={styles["mobile-nav-btn"]}
                         >
-                            <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
-                        </svg>
-                    </button>
-                )}
-            </nav>
-        </header>
+                            <svg
+                                width={30}
+                                height={30}
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 448 512"
+                            >
+                                <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
+                            </svg>
+                        </button>
+                    )}
+                </nav>
+            </header>
+            {isMobile && (
+                <MobileNav
+                    isMobileNavVisible={isMobileNavVisible}
+                    mobileNavHandler={mobileNavHandler}
+                    userData={userData}
+                />
+            )}
+        </>
     );
 };
--- client/src/Components/components/Core/Header/MobileNav/MobileNav.js
+++ client/src/Components/components/Core/Header/MobileNav/MobileNav.js
@@ -2,26 +2,60 @@ import { Link } from "react-router-dom";
 
 import styles from "../Header.module.css";
 
-export const MobileNav = () => {
+export const MobileNav = ({
+    userData,
+    isMobileNavVisible,
+    mobileNavHandler,
+}) => {
+    const className = isMobileNavVisible ? "show" : "hide";
+    console.log(className);
     return (
-        <aside className={styles["mobile-nav"]}>
-            <div className={styles["user-container"]}></div>
+        <aside className={`${styles["mobile-nav"]} ${styles[className]}`}>
+            <button
+                onClick={mobileNavHandler}
+                type="button"
+                className={styles["close-mobile-nav"]}
+            >
+                <svg
+                    width={30}
+                    height={30}
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 320 512"
+                >
+                    <path d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z" />
+                </svg>
+            </button>
+            <header className={styles["user-container"]}>
+                <div className={styles["user-wrapper"]}>
+                    <h2 className={styles["title"]}>
+                        <span className={styles["greeting"]}>Hello, </span>{" "}
+                        <span className={styles["username"]}>
+                            Userrrrrrrrrrrrrrrrrrrrrr...
+                        </span>
+                    </h2>
+                    <img
+                        className={styles["user-img"]}
+                        src="/user-default.png"
+                        alt=""
+                    />
+                </div>
+            </header>
             <div className={styles["links-container"]}>
                 <ul className={styles["list"]}>
                     <li className={styles["item"]}>
-                        <Link></Link>
+                        <Link>Some link</Link>
                     </li>
                     <li className={styles["item"]}>
-                        <Link></Link>
+                        <Link>Some link</Link>
                     </li>
                     <li className={styles["item"]}>
-                        <Link></Link>
+                        <Link>Some link</Link>
                     </li>
                     <li className={styles["item"]}>
-                        <Link></Link>
+                        <Link>Some link</Link>
                     </li>
                     <li className={styles["item"]}>
-                        <Link></Link>
+                        <Link>Some link</Link>
                     </li>
                 </ul>
             </div>
