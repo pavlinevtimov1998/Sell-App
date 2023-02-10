1f00c4b10eda132f07de7621553965dde214c7ec
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-02-10 13:24:18 +0200

    Update Header.js

--- client/src/Components/components/Core/Header/Header.js
+++ client/src/Components/components/Core/Header/Header.js
@@ -1,7 +1,8 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../../../Contexts/AuthContext";
 import { useHeaderVisibility } from "../../../../Hooks/useHeaderVisibility";
+import { useResize } from "../../../../Hooks/useResize";
 
 import styles from "./Header.module.css";
 
@@ -10,6 +11,7 @@ import { UserOptions } from "./UserNav/UserOptions";
 
 export const Header = () => {
     const { isHeaderVisible } = useHeaderVisibility();
+    const { state: isMobile } = useResize(600);
     const { userData } = useContext(AuthContext);
 
     const headerClass = isHeaderVisible ? "show" : "hide";
@@ -22,7 +24,20 @@ export const Header = () => {
                         <img src="/logo.png" alt="LOGO" />
                     </Link>
                 </article>
-                {userData ? <UserOptions /> : <GuestOptions />}
+                {userData && !isMobile && <UserOptions />}
+                {!userData && !isMobile && <GuestOptions />}
+                {isMobile && (
+                    <button type="button" className={styles["mobile-nav-btn"]}>
+                        <svg
+                            width={30}
+                            height={30}
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 448 512"
+                        >
+                            <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
+                        </svg>
+                    </button>
+                )}
             </nav>
         </header>
     );
