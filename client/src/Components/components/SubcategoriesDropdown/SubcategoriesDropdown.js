72207482cb4b01af17c00f553b59d424ec8d2403
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-02-16 14:31:08 +0200

    Update SubcategoriesDropdown.js

--- client/src/Components/components/SubcategoriesDropdown/SubcategoriesDropdown.js
+++ client/src/Components/components/SubcategoriesDropdown/SubcategoriesDropdown.js
@@ -2,17 +2,29 @@ import styles from "./SubcategoriesDropdown.module.css";
 
 import { Link } from "react-router-dom";
 
-export const SubcategoriesDropdown = ({ subcategories, isVisible }) => {
+export const SubcategoriesDropdown = ({
+    categoryTitle,
+    subcategories,
+    isVisible,
+}) => {
     return (
         <article
             className={`${styles["subcategories-dropdown"]}  ${styles[isVisible]}`}
         >
             <div className={styles["subcat-container"]}>
+                <Link
+                    className={styles["subcategory"]}
+                    title={`See everything in ${categoryTitle}`}
+                    to={`/listing?category=${categoryTitle}`}
+                >
+                    See evereything in {categoryTitle}
+                </Link>
                 {subcategories.map((s) => (
                     <Link
                         className={styles["subcategory"]}
                         key={s._id}
                         title={s.title}
+                        to={`/listing?category=${categoryTitle}&subcategory=${s.title}`}
                     >
                         {s.title}
                     </Link>
